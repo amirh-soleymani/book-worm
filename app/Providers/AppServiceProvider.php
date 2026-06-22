4d818e511fc2ddf4eb3666fd1f7d25b6af7799a0
Amir <royal.falconalex@gmail.com>
2026-06-23 02:28:58 +0330

    feat(response): add success and fail helper methods to Response class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Response::macro('success', function (
+            mixed $data = null,
+            string $message = 'Success',
+            int $status = 200
+        ) {
+            return Response::json([
+                'success' => true,
+                'message' => $message,
+                'data' => $data,
+            ], $status);
+        });
+
+        Response::macro('failure', function (
+            string $message = 'Something went wrong',
+            int $status = 400,
+            mixed $errors = null
+        ) {
+            return Response::json([
+                'success' => false,
+                'message' => $message,
+                'errors' => $errors,
+            ], $status);
+        });
     }
 }
